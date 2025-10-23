cb8fdc1c800e7cef460755af67ce4a45b36d87cd
pauls <paul.speier@web.de>
2025-10-23 15:05:20 +0200

    Done :)

--- calc.py
+++ calc.py
@@ -1,6 +1,25 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    
+    print("Enter operation (add/addition or multi/multiplication): ")
+    operation = input().strip().lower()
+    
+    if operation in ["add", "addition"]:
+        result = num1 + num2
+        print("Sum: " + str(result))
+    elif operation in ["multi", "multiplication"]:
+        result = num1 * num2
+        print("Product: " + str(result))
+    else:
+        print("Invalid operation!")
+        continue
+    
+    print("Do you want to continue? (yes/y to continue, anything else to exit): ")
+    continue_choice = input().strip().lower()
+    
+    if continue_choice not in ["yes", "y"]:
+        print("Goodbye!")
+        break
