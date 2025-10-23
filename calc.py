21c0de17aad3d2348526595f9b8e104fe712c885
moritz <moritzleo07@gmail.com>
2025-10-23 12:50:32 +0200

    Solution soluted

--- calc.py
+++ calc.py
@@ -1,6 +1,25 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def calc():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter operation (+/a/add/addition or */x/m/mult/multiplication) ")
+    operation = input()
+
+    if operation in ["+", "a", "add", "addition"]:
+        print("Sum: " + str(num1+num2))
+    elif operation in ["*", "x", "m", "mult", "multiplication"]:
+        print("Product: " + str(num1*num2))
+    else:
+        print("Invalid operation")
+
+    print("Calculation complete.")
+    print("Do you want to perform another calculation? (y/n)")
+    choice = input()
+    if choice.lower() == "y":
+        calc()
+    else:
+        print("Exiting calculator.")
+
+
+calc()
