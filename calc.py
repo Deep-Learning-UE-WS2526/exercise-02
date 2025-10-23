486f5201323e647e0451dae2f142c0a0cd0f8ca9
limploui <llimper@smail.uni-koeln.de>
2025-10-23 12:55:14 +0200

    Limper exercise 2

--- calc.py
+++ calc.py
@@ -1,6 +1,24 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def calc():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter the operation (x, multi/multiplication for multiplication OR +, add/addition for addition): ")
+
+    operation = str(input())
+    if operation == "x" or operation == "multi" or operation == "multiplication":
+        print("Product: " + str(num1*num2))
+    if operation == "+" or operation == "add" or operation == "addition":
+        print("Sum: " + str(num1+num2))
+    abfrage()
+    
+
+def abfrage():
+    print ("Exit? (Yes OR No): ")
+    weiter = str(input())
+    if weiter == "yes":
+        calc()
+    if weiter == "no":
+        print ("Mach et jot!")
+
+calc()
