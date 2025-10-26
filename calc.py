f1f83e940ba68666600feec9c299388a346897aa
Flor <florcasaleiro@gmail.com>
2025-10-26 11:51:03 +0100

    exercise 2

--- calc.py
+++ calc.py
@@ -1,6 +1,27 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def exercise():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    
+    # 1
+    operation = input("Addition or multiplication? ")
+    
+    # 2
+    if operation == "add" or operation == "addition":
+        print("Sum: " + str(num1+num2))
+    elif operation == "multi" or operation == "multiplication":
+        print("Product: " + str(num1*num2))
+    else:
+        print("Invalid operation.")
+    
+def main():
+    # 3
+    while True:
+        exercise()
+        continuation = input("Do you wish to continue? (y/n) ")
+        if continuation != "y":
+            break
+            
+if __name__ == "__main__":
+    main()
