bc00c08726efd11d080e2e3f3fc4381c182d8536
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-10-28 21:15:27 +0100

    finished exercise

--- calc.py
+++ calc.py
@@ -1,6 +1,32 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+import operator
+ops = {
+    "+": operator.add,
+    "add": operator.add,
+    "addition": operator.add,
+    "*": operator.mul,
+    "multi": operator.mul,
+    "multiplication": operator.mul
+}
+def start():
+    print("Enter number 1: ")
+    num1 = int(input())
+
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    print("Enter operator: ")
+    opInput = str(input())
+
+    op = ops.get(opInput)
+    print("Solution: "+ str(op(num1, num2)))
+
+    print("Do you want to continue (y/n)?")
+    confirm = input()
+    if confirm == "y":
+        print("Starting over..")
+        start()
+    elif confirm == "n":
+        print("exiting..")
+        SystemExit
+
+start()
