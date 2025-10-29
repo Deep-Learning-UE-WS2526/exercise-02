bb9ee9e70051365361de79585427a57e0246f693
mar0303 <scherfmarilis@gmail.com>
2025-10-29 21:53:11 +0100

    Aufgabe gelöst

--- calc.py
+++ calc.py
@@ -1,6 +1,17 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+on = True
+while on:  
+    on = False
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter operation (mulitiplication/ mutli/ * or addition/ add/ +): ")
+    op = input()
+    if op == "multiplication" or op == "multi" or op == "*":
+        print("Product: " + str(num1*num2))
+    if op == "addition" or op == "add" or op == "+":
+        print("Sum: " + str(num1+num2))
+    print("To continue, enter y.")
+    on = input() == "y"
+
+    
